e911d5f6a17619215dd5a684faf7489ebe6167f8
DhruvJha707 <dhruvjha707@gmail.com>
2026-02-04 00:17:40 +0530

    Initial commit

---
+++ exp3q6.py
@@ -0,0 +1,12 @@
+#Write a program to print sum of digits.
+
+num = int(input("Enter a number: "))
+
+sum_of_digits = 0
+
+while num > 0:
+    digit = num % 10
+    sum_of_digits += digit
+    num //= 10
+
+print("Sum of digits:", sum_of_digits)
