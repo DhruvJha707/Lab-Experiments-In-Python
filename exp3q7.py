3275b4a63594c1f7a9c19e6f1a75c149752c1972
DhruvJha707 <dhruvjha707@gmail.com>
2026-02-04 00:17:47 +0530

    Initial commit

---
+++ exp3q7.py
@@ -0,0 +1,13 @@
+#Count and print all numbers divisible by 5 or 7 between 1 to 100.
+
+
+count = 0
+
+print("Numbers divisible by 5 or 7 between 1 and 100:")
+
+for i in range(1, 101):
+    if i % 5 == 0 or i % 7 == 0:
+        print(i, end=" ")
+        count += 1
+
+print("\nTotal count:", count)
