200d599dae219422fe77c3fc30d18c54427b3a8f
DhruvJha707 <dhruvjha707@gmail.com>
2026-02-06 21:21:18 +0530

    Initial commit

---
+++ exp4q2.py
@@ -0,0 +1,12 @@
+# Count total number of vowels in a given string.
+
+string = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in string:
+    if char in vowels:
+        count += 1
+
+print("Total number of vowels:", count)
