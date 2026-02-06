dc83caf201a89575b77f3c7583353b9ff70ce6d7
DhruvJha707 <dhruvjha707@gmail.com>
2026-02-06 21:21:10 +0530

    Initial commit

---
+++ exp4q1.py
@@ -0,0 +1,11 @@
+# Write a program to count and display the number of capital letters in a given string.
+
+
+text = input("Enter a string: ")
+
+count = 0
+for char in text:
+    if char.isupper():
+        count += 1
+
+print("Number of capital letters:", count)
