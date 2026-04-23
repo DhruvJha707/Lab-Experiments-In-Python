d4fa83d87237a809693db2f9bbcde713d3fe378a
DhruvJha707 <dhruvjha707@gmail.com>
2026-04-23 08:50:44 +0530

    Initial commit

---
+++ exp10q1.py
@@ -0,0 +1,10 @@
+''' Create numpy array to find sum of all elements in an array. '''
+
+import numpy as np
+
+
+arr = np.array([1, 2, 3, 4, 5])
+
+total_sum = np.sum(arr)
+print("The sum of all elements in the array is:", total_sum)
+
